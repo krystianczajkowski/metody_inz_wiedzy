ca94e3ccad15241122f15e865fb2770405bffcd0
Krystian Czajkowski <krystianczajkowski01@gmail.com>
2026-03-04 21:42:27 +0100

    Begin work on sequential covering

--- labs/lab1/sequential_covering.py
+++ labs/lab1/sequential_covering.py
@@ -1,8 +1,40 @@
 from itertools import combinations
 
 # 3 pętle
+# wczytywanie systemu decyzyjnego z pliku
 
 
-def sequential_covering():
+def pretty_print_dict(objects):
+    print("     a1,  a2,  a3,  a4,  a5,  a6    d")
+    for k, v in objects.items():
+        print(k, v)
 
-    pass
+
+def obtain_objects_dict(fname):
+    file = open(fname, "r")
+    objects = dict()
+    for i, line in enumerate(file.readlines(), start=1):
+        objects[f"ob{i}"] = line.split()
+    file.close()
+    return objects
+
+
+def obtain_objects_list(fname):
+    file = open(fname, "r")
+    objects = []
+    for i, line in enumerate(file.readlines(), start=1):
+        objects.append(line.split())
+    file.close()
+    return objects
+
+
+def sequential_covering(objects):
+    pretty_print_dict(objects)
+
+
+def main():
+    sequential_covering(obtain_objects_dict("sys_dec"))
+
+
+if __name__ == "__main__":
+    main()
